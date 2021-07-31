198c3141172a75e79fc90e0b4f9a4418b6049855
utgwkk <utagawakiki@gmail.com>
2021-07-31 15:42:25 +0900

    use DynamoDB as StatusStore backend

--- status_store.go
+++ status_store.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"github.com/syndtr/goleveldb/leveldb"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 // StatusStore represents current status cache store
 type StatusStore struct {
-	databaseFile string
+	dynamoDB *dynamodb.DynamoDB
 }
+
+var awsSession = session.Must(session.NewSession())
+var tableName = aws.String("ZatsuMonitor")
 
 const (
 	// NotFoundKey represents value if key is not found
@@ -19,42 +24,55 @@ const (
 // NewStatusStore create new StatusStore instance
 func NewStatusStore(databaseFile string) *StatusStore {
 	s := new(StatusStore)
-	s.databaseFile = databaseFile
+	s.dynamoDB = dynamodb.New(awsSession)
 	return s
 }
 
 // GetDbStatus returns status code for specified key
 func (s *StatusStore) GetDbStatus(key string) (int, error) {
-	db, err := leveldb.OpenFile(s.databaseFile, nil)
+	result, err := s.dynamoDB.GetItem(&dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"Key": {
+				S: aws.String(key),
+			},
+		},
+		TableName: tableName,
+	})
 	if err != nil {
 		return 0, err
 	}
-	defer db.Close()
 
-	if ret, _ := db.Has([]byte(key), nil); !ret {
+	if result == nil {
 		return NotFoundKey, nil
 	}
 
-	data, err := db.Get([]byte(key), nil)
+	statusCode, ok := result.Item["status_code"]
+	if !ok {
+		return NotFoundKey, nil
+	}
 
-	buf := bytes.NewBuffer(data)
-	statusCode, _ := binary.Varint(buf.Bytes())
-
-	return int(statusCode), nil
+	return strconv.Atoi(*statusCode.N)
 }
 
 // SaveDbStatus saves status code for specified key
 func (s *StatusStore) SaveDbStatus(key string, statusCode int) error {
-	db, err := leveldb.OpenFile(s.databaseFile, nil)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	buf := make([]byte, binary.MaxVarintLen32)
-	binary.PutVarint(buf, int64(statusCode))
-
-	db.Put([]byte(key), buf, nil)
-
-	return nil
+	statusCodeStr := strconv.Itoa(statusCode)
+	_, err := s.dynamoDB.UpdateItem(&dynamodb.UpdateItemInput{
+		ExpressionAttributeNames: map[string]*string{
+			"#S": aws.String("StatusCode"),
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":s": {
+				N: aws.String(statusCodeStr),
+			},
+		},
+		Key: map[string]*dynamodb.AttributeValue{
+			"Key": {
+				S: aws.String(key),
+			},
+		},
+		UpdateExpression: aws.String("SET #S = :s"),
+		TableName:        tableName,
+	})
+	return err
 }
